a102296d785d89a1c9b1ac6a4a8ecd2748db68f6
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-08-09 16:37:55 -0300

    highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,6 @@
+(fork (label) @function) @keyword
+(join (label) @function (label) @function (label) @function) @keyword
+"FORK" @keyword
+(def (label)) @function
+(call (label)) @type
+(assign (label) @property (digit) @string)
